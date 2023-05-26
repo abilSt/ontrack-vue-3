33e63ccee6ebe7d17d6d80e36c413af594318e0e
abilSt <sabytahunov@mail.ru>
2023-05-26 18:18:50 +0600

    generateActivies

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR } from './constants'
+import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR, SECONDS_IN_HOUR } from './constants'
 import { isPageValid } from './validators'
 
 export function normalizePageHash() {
@@ -13,6 +13,18 @@ export function normalizePageHash() {
   return PAGE_TIMELINE
 }
 
+export function generateActivities() {
+  return [
+    { id: id(), name: 'Coding', secondsComplete: 0 * SECONDS_IN_HOUR },
+    { id: id(), name: 'Training', secondsComplete: 1 * SECONDS_IN_HOUR },
+    { id: id(), name: 'Reading', secondsComplete: 2 * SECONDS_IN_HOUR }
+  ]
+}
+
+export function id() {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2)
+}
+
 export function generateTimelineItems() {
   const timelineItems = []
 
@@ -24,5 +36,5 @@ export function generateTimelineItems() {
 }
 
 export function generateActivitySelectOptions(activities) {
-  return activities.map((label, value) => ({ label, value }))
+  return activities.map((activity) => ({ label: activity.name, value: activity.id }))
 }
