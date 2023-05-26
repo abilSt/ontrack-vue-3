e280d98910484c12bdbef92ed12caaf89f8332e3
abilSt <sabytahunov@mail.ru>
2023-05-26 18:02:40 +0600

    TheActivitiesEmptyState

---
+++ src/components/TheActivitiesEmptyState.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text center flex grow flex-col items-center gap-4 p-8">
+    <img src="../assets/img/no_activities.svg" alt="No activities" class="h-48" />
+    <span class="text-gray-400">You don`t have any activities</span>
+  </div>
+</template>
